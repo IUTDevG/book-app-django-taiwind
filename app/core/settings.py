b43f27b108631acee9a89b29242df921b9fa677c
adr-2 <www.adriensasni@gmail.com>
2025-04-16 23:42:50 +0100

    collectstatic

--- app/core/settings.py
+++ app/core/settings.py
@@ -151,6 +151,8 @@ STATICFILES_DIRS = [
 ]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR/'media'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
---
+++ app/staticfiles/main.js
@@ -0,0 +1,6 @@
+const head = document.getElementById('head')
+
+if (head){
+    const goBackBtn = document.getElementById('go-back-btn')
+    goBackBtn.addEventListener('click',()=>history.back())
+}
